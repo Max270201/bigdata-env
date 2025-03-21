874ba2e3e5be5afd48ba75de04b0f04ebc51c9c1
Max270201 <martino.solito01@universitadipavia.it>
2025-03-21 17:13:13 +0000

    updatets to my notes

---
+++ class-bigdata/My notes/from_z-score_to_pvalue.R
@@ -0,0 +1,119 @@
+library(tidyverse)
+
+############################
+### score of exams ########
+
+s = 0:35 # possible scores of the exam
+joe = 30 # score of our test student
+pop_mean = 25 # mean score of most students at exam
+pop_sd = 4 # standard deviation of exam results
+alpha = 0.05 # we need to choose our significance level
+
+## create a tibble with probability associated to each possible score
+## from a normal distribution with these characteristics
+
+normalDensity = tibble(
+  score = s,
+  probability = dnorm ##density probability out of normal distribution
+  (s, mean = pop_mean, sd = pop_sd) ## probability of achieving that score with normal mean 25 & sd 4
+)
+
+## we can now order the values of the probability in order to compute
+## the cumulative probability
+
+normalDensity = normalDensity %>%
+  arrange(probability) %>% ## to make sure data are ordered by probability first
+  mutate(
+    cumulative_p = cumsum ##cumulative sum to calculate the cumulative value of all previous value
+    ##every prob is now the sum of everything that comez before that
+    (probability), ## now they are ordered we can make a cumulative p
+    reject = cumulative_p <= alpha
+  )  ## boulean to say that if cump is equal or lower alpha then reject is true
+## 
+
+## now we can plot this
+
+normalplot = ggplot(normalDensity)+
+  ##first layer adds bars
+  geom_bar(aes(x=score, y=probability, fill = reject##colour you want to fill the bar with, using the variable you created "reject"
+               ), stat = "identity")+
+  scale_fill_manual(
+    values = c(
+      `TRUE` = "red",
+      `FALSE` = "darkgrey" ##this is how to change colours
+    )
+  )+
+  geom_vline(xintercept = joe, col = "blue")+ ##adding vertical line, geoms stands for geometry
+  theme(legend.position = "none")
+
+plot(normalplot)
+##you can see the normal distr of all possible values, with a certain mean and sdev, in red all cumulative prob
+##that are below 0,05, or your specific significance level
+
+
+## if we consider the sum of the probabilities of all the scores higher than
+## our student, we obtain
+
+sum(
+  normalDensity$probability[normalDensity$score>=joe]
+)
+##which is 12 percent of your students that would perform better (in a normal distr)
+
+### for a proper test statistic we compute a relative standing
+### using a z-score, which in this case would be
+
+z_score = (joe - pop_mean) / pop_sd
+
+
+## the p-value is then calculated as the proportion of probability
+## equal or above the z-score of our single measure
+## on a default normal distribution (i.e. mean = 0 and sd = 1)
+
+pnorm(z_score, lower.tail = FALSE)
+
+
+## roughly it still represents the proportion we have seen above
+
+
+
+###############################################
+## why z-score plotted on a normal dist?
+##############################################
+
+## our null hypothesis is NOT the distribution of scores
+## but the distribution of z-scores
+## we can compute it as below:
+
+## pack z-score calculation in a function
+
+zcalc <- function(x){
+  z = (x-pop_mean)/pop_sd
+  return(z)
+}
+
+
+## create a tibble with real scores (not their probability)
+## drawn out of the same normal distribution
+
+exam_sample = tibble(
+  scores = rnorm(2000, mean = pop_mean, sd = pop_sd)
+) %>%
+  mutate(
+    zscore = zcalc(scores) ## then we calculate the z-score of each exam score
+  ) %>%
+  arrange(zscore)
+
+## we can plot it, and measure against our z-score
+
+zplot = ggplot(exam_sample)+
+  geom_bar(aes(x=zscore), stat = "density")+
+  geom_vline(xintercept = z_score, col = "red")
+
+plot(zplot)
+
+### if we plot the two one below the other we can see they're distributed in a pretty
+### similar way
+
+library(gridExtra)
+grid.arrange(normalplot+xlim(10,38), zplot, nrow=2)
+
---
+++ class-bigdata/My notes/notes.R
@@ -0,0 +1,47 @@
+STATISTICAL INFERENCE
+making deductions, population is the whole package, you use samples to get info on the pop
+
+Estimation: estimating the population mean through the sample mean (with an error), statistic info taken from the sample
+- point est <- single number to use a population parameter
+- interval est <- 2 values to estimate, a pop parameter is within that interval (with a level of confidence)
+
+standard error:the smaller the sample you work with, the higher the error (standard dev/squared root of sample)
+also, 1,96StandError gives you the confidence interval
+
+HYpo testing: first, you need to define a few things
+H0 <- null hypo, what would happen if your alternative hypo was FALSE
+Ha <- alternative hypo, what you are testing for
+test statistics <- es. p value
+significance level <- what is the value from which I draw the threshold for the rejection on null hypo (usually 0,05)
+it is your personal decision to put it this value
+
+P-VALUE:
+the percentage of risk you are willing to take in order to reject H0 (the lower, the less error you are willing to accept)
+
+having a given distribution, everything that lies below the curve represents possible values for your z score.
+In this way, given a certain z-score value you have  all the possible values that are above or below (??)
+
+in the normal distribution in the slides: p-value is the proportion of values that are above my z-score 
+probability because it is referring to the frequency in the whole population
+so, you can say that it is a probability because it represents how much it is probable that I find
+higher values than my z-score (if H0 was true, which is the risl that i prepare to accept)
+
+R DEMO (see from z score to p value file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
